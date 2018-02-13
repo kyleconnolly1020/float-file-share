3ceacb2fa1261b602997d2529aac6100315c5251
Kyle <kyleconnolly1020@gmail.com>
2018-02-13 13:31:59 -0800

    react-router-dom implementation

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,23 +1,25 @@
 import React from "react";
-import "./NavBar.css"; 
+import "./NavBar.css";
 
 import { Link } from "react-router-dom";
 
-class NavBar extends React.Component{
-    render (props) {
-        return(
+class NavBar extends React.Component {
+    render(props) {
+        return (
             <nav className="navbar navbar-default">
                 <a className="navbar-brand">
                     <span className="glyphicon glyphicon-menu-hamburger"></span>
                 </a>
                 <span className="location">{this.props.location}</span>
-                <a className="navbar-brand navbar-right">
-                    <span className="glyphicon glyphicon-user"></span>
-                </a>
+                <Link to='/profile'>
+                    <span className="navbar-brand navbar-right">
+                        <span className="glyphicon glyphicon-user"></span>
+                    </span>
+                </Link>
             </nav>
         );
     }
-    
+
 }
 
 export default NavBar;
