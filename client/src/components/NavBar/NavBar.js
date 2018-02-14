8aafd27323a247c918eebcd060c4bd2ed0462d4e
Kyle <kyleconnolly1020@gmail.com>
2018-02-14 12:15:18 -0800

    added search bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./NavBar.css";
-
+import Search from "../Search";
 import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
     render(props) {
         return (
             <nav className="navbar navbar-default">
-                <a className="navbar-brand">
-                    <span className="glyphicon glyphicon-menu-hamburger"></span>
-                </a>
+                <Search/>
                 <span className="location">{this.props.location}</span>
 
                 <span className="navbar-brand navbar-right">
