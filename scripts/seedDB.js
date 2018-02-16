2890c810a3bfea7747a65a416e37c6d2a74941e8
Ben <ben.harloe@gmail.com>
2018-02-16 14:28:19 -0800

    functioning files collection, seed script, and populate in API

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -8,7 +8,7 @@ const userSeed = [
   {
     "username": "bharloe",
     "location": {
-      "coordinates": [-117.184242, 32.853822], 
+      "coordinates": [-117.184242, 32.853822],
       "type": "Point"
     },
     "socialProfiles": {
@@ -19,13 +19,13 @@ const userSeed = [
       "pdf": "pdf_url",
       "audiofile": "audio_url"
     },
-    "image" : "https://image.flaticon.com/icons/svg/145/145867.svg",
+    "image": "https://image.flaticon.com/icons/svg/145/145867.svg",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim feugiat sodales. Donec finibus urna eget lobortis aliquam. Nunc elementum sollicitudin sagittis."
   },
   {
     "username": "kyleconnolly",
     "location": {
-      "coordinates": [-117.183268, 32.853431], 
+      "coordinates": [-117.183268, 32.853431],
       "type": "Point"
     },
     "socialProfiles": {
@@ -41,7 +41,7 @@ const userSeed = [
   {
     "username": "johnm",
     "location": {
-      "coordinates": [-117.184637, 32.852760], 
+      "coordinates": [-117.184637, 32.852760],
       "type": "Point"
     },
     "socialProfiles": {
@@ -56,7 +56,7 @@ const userSeed = [
   {
     "username": "kevin",
     "location": {
-      "coordinates": [-117.188189, 32.854047], 
+      "coordinates": [-117.188189, 32.854047],
       "type": "Point"
     },
     "socialProfiles": {
@@ -73,7 +73,7 @@ const userSeed = [
   {
     "username": "Clark",
     "location": {
-      "coordinates": [-117.187062, 32.851830], 
+      "coordinates": [-117.187062, 32.851830],
       "type": "Point"
     },
     "socialProfiles": {
@@ -92,7 +92,7 @@ const userSeed = [
   {
     "username": "TheTravMan",
     "location": {
-      "coordinates": [-117.204316, 32.854476], 
+      "coordinates": [-117.204316, 32.854476],
       "type": "Point"
     },
     "socialProfiles": {
@@ -109,7 +109,7 @@ const userSeed = [
   {
     "username": "David",
     "location": {
-      "coordinates": [-117.234294, 32.849433], 
+      "coordinates": [-117.234294, 32.849433],
       "type": "Point"
     },
     "socialProfiles": {
@@ -126,7 +126,7 @@ const userSeed = [
   {
     "username": "johnd",
     "location": {
-      "coordinates": [-117.273001, 32.851099], 
+      "coordinates": [-117.273001, 32.851099],
       "type": "Point"
     },
     "savedFiles": {
@@ -142,13 +142,68 @@ const userSeed = [
   }
 ];
 
-db.User.remove({})
+const filesSeed = [
+  {
+    username: "bharloe",
+    file: {
+      url: "https://s3.amazonaws.com/floatfileshare/learnwebdevelopmentwithvegibit.pdf",
+      filetype: "pdf",
+      description: ""
+    }
+  },
+  {
+    username: "bharloe",
+    file: {
+      url: "https://s3.amazonaws.com/floatfileshare/nodebeginner.pdf",
+      filetype: "pdf",
+      description: ""
+    }
+  },
+  {
+    username: "kyleconnolly",
+    file: {
+      url: "https://s3.amazonaws.com/floatfileshare/nodebeginner.pdf",
+      filetype: "pdf",
+      description: ""
+    }
+  },
+  {
+    username: "johnd",
+    file: {
+      url: "https://s3.amazonaws.com/floatfileshare/nodecraftsman.pdf",
+      filetype: "pdf",
+      description: ""
+    }
+  }
+]
+
+db.File.remove({}).then(() => db.User.remove({})
   .then(() => db.User.collection.insertMany(userSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
+    console.log(data.result.n + " records inserted");
+    for (let i = 0; i < filesSeed.length; i++) {
+      // Create a new Note in the db
+      db.File.create(filesSeed[i].file)
+        .then(function (dbFile) {
+          return db.User.findOneAndUpdate({ "username": filesSeed[i].username }, { $push: { files: dbFile._id } }, { new: true });
+        })
+        .then(function (dbUser) {
+          // If the File was updated successfully, log it
+          if (i === filesSeed.length - 1) {
+            console.log(i +" files inserted");
+            process.exit(0);
+          }
+        })
+        .catch(function (err) {
+          // If an error occurs, log it
+          console.log(err);
+        });
+    }
   })
   .catch(err => {
     console.error(err);
     process.exit(1);
-  });
+  })
+);
+
+
