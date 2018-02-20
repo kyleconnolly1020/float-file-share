6bfe9326a0325749d491cd37c2553922e3122091
Clark <clarkphan@github.com>
2018-02-20 10:13:02 -0800

    change max distance

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -26,7 +26,7 @@ module.exports = {
           },
           //SET DISTANCE FOR $near QUERY HERE
           //Distance in meters
-          $maxDistance: 50000,
+          $maxDistance: 500000,
           $minDistance: 0
         }
       }
