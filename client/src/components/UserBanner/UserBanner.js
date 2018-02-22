1dcfbd1df02dd0423825ef8e29617ba0df14793f
Kyle <kyleconnolly1020@gmail.com>
2018-02-21 21:08:20 -0800

    Added Update Button to profile js

--- client/src/components/UserBanner/UserBanner.js
+++ client/src/components/UserBanner/UserBanner.js
@@ -9,18 +9,27 @@ import {
     HelpBlock,
     ControlLabel,
     Button
-  } from "react-bootstrap";
+} from "react-bootstrap";
 
 class UserBanner extends React.Component {
     state = {
-        open: false
+        open: false,
+        update: false
     }
     handler = () => {
         this.setState({
             open: !this.state.open
         });
     }
+
+    formSwitch = () => {
+        this.setState({
+            update: !this.state.update
+        });
+    }
+
     render() {
+        console.log(this.state);
         return (
             <div>
                 <Panel id="collapsible-panel-example-1" expanded={this.props.open ? this.props.open : this.state.open} >
@@ -44,12 +53,12 @@ class UserBanner extends React.Component {
                         <Panel.Body>
                             <div className="row">
                                 <div className="col-md-6">
-                                    {<p><strong>{this.props.description}</strong></p>}
-                                    {this.props.userSocials.facebook ? (<p className="facebook"><span><i className='fab fa-facebook-square'></i></span><a href={this.props.userSocials.facebook} target="_blank">{'  ' + this.props.userSocials.facebook}</a></p>) : null}
-                                    {this.props.userSocials.twitter ? (<p className="twitter"><span><i className='fab fa-twitter'></i></span><a href={this.props.userSocials.twitter} target="_blank">{'  ' + this.props.userSocials.twitter}</a></p>) : null}
-                                    {this.props.userSocials.snapchat ? (<p className="snapchat"><span><i className='fab fa-snapchat-ghost'></i></span>{'  ' + this.props.userSocials.snapchat}</p>) : null}
-                                    {this.props.userSocials.linkedin ? (<p className="linkedin"><span><i className='fab fa-linkedin'></i></span><a href={this.props.userSocials.linkedin} target="_blank">{'  ' + this.props.userSocials.linkedin}</a></p>) : null}
-                                    {this.props.userSocials.instagram ? (<p className="instagram"><span><i className='fab fa-instagram'></i></span><a href={this.props.userSocials.instagram} target="_blank">{'  ' + this.props.userSocials.instagram}</a></p>) : null}
+                                    {this.state.update ? (<input type="text" defaultValue={this.props.description} />) : (<p><strong>{this.props.description}</strong></p>)}
+                                    {this.state.update ? (<p className="facebook"><span><i className='fab fa-facebook-square'></i> </span><input type="text" defaultValue="facebook.com/"/></p>) : (this.props.userSocials.facebook ? (<p className="facebook"><span><i className='fab fa-facebook-square'></i></span><a href={this.props.userSocials.facebook} target="_blank">{'  ' + this.props.userSocials.facebook}</a></p>) : null)}
+                                    {this.state.update ? (<p className="twitter"><span><i className='fab fa-twitter'></i> </span><input type="text" defaultValue="twitter.com/"/></p>) : (this.props.userSocials.twitter ? (<p className="twitter"><span><i className='fab fa-twitter'></i></span><a href={this.props.userSocials.twitter} target="_blank">{'  ' + this.props.userSocials.twitter}</a></p>) : null)}
+                                    {this.state.update ? (<p className="snapchat"><span><i className='fab fa-snapchat-ghost'></i> </span><input type="text" defaultValue={this.props.userSocials.snapchat}/></p>) : (this.props.userSocials.snapchat ? (<p className="snapchat"><span><i className='fab fa-snapchat-ghost'></i></span><a href={this.props.userSocials.snapchat} target="_blank">{'  ' + this.props.userSocials.snapchat}</a></p>) : null)}
+                                    {this.state.update ? (<p className="linkedin"><span><i className='fab fa-linkedin'></i> </span><input type="text" defaultValue="linkedin.com/"/></p>) : (this.props.userSocials.linkedin ? (<p className="linkedin"><span><i className='fab fa-linkedin'></i></span><a href={this.props.userSocials.linkedin} target="_blank">{'  ' + this.props.userSocials.linkedin}</a></p>) : null)}
+                                    {this.state.update ? (<p className="instagram"><span><i className='fab fa-instagram'></i> </span><input type="text" defaultValue={this.props.userSocials.instagram}/></p>) : (this.props.userSocials.instagram ? (<p className="instagram"><span><i className='fab fa-instagram'></i></span><a href={this.props.userSocials.instagram} target="_blank">{'  ' + this.props.userSocials.instagram}</a></p>) : null)}
                                 </div>
                                 <div className="col-md-6">
                                     {this.props.userFiles.map(file => {
@@ -69,16 +78,17 @@ class UserBanner extends React.Component {
                                 </div>
                             </div>
                             {this.props.open &&
-                            <form onSubmit={this.props.handleFormSubmit} onChange={this.props.handleInputChange} className="text-center">
-                                <FormGroup controlId="formControlsFile">
-                                    <ControlLabel>Upload File</ControlLabel>
-                                    <div id="form">
-                                    <FormControl type="file" name="file"/>
-                                    </div>
-                                    <HelpBlock>Select a file you would like to upload</HelpBlock>
-                                </FormGroup>
-                                <Button type="submit">Submit</Button>
-                            </form>
+                                <form onSubmit={this.props.handleFormSubmit} onChange={this.props.handleInputChange} className="text-center">
+                                    <FormGroup controlId="formControlsFile">
+                                        <ControlLabel>Upload File</ControlLabel>
+                                        <div id="form">
+                                            <FormControl type="file" name="file" />
+                                        </div>
+                                        <HelpBlock>Select a file you would like to upload</HelpBlock>
+                                    </FormGroup>
+                                    <Button id="submitButton" type="submit">Submit</Button>
+                                    <Button style={{ float: "right" }} onClick={this.formSwitch}>Update Profile</Button>
+                                </form>
                             }
                         </Panel.Body>
                     </Panel.Collapse>
